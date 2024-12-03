d0797bd980317f3862300edf765464dd5c7e0951
code043 <procoder043@gmail.com>
2024-12-03 16:57:42 -0300

    Create a order

--- application/routes.go
+++ application/routes.go
@@ -4,21 +4,26 @@ import (
 	"net/http"
 
 	"github.com/code043/chi-api/handler"
+	"github.com/code043/chi-api/repository/order"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+	router.Route("/orders", a.loadOrderRoutes)
+	a.router = router
 }
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
 	router.Get("/{id}", orderHandler.GetById)
--- handler/order.go
+++ handler/order.go
@@ -1,14 +1,52 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/code043/chi-api/model"
+	"github.com/code043/chi-api/repository/order"
+	"github.com/google/uuid"
 )
 
-type Order struct{}
+type Order struct {
+	Repo *order.RedisRepo
+}
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create an order")
+func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		CustomerId uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	now := time.Now().UTC()
+
+	order := model.Order{
+		OrderId:    rand.Uint64(),
+		CustomerId: body.CustomerId,
+		LineItems:  body.LineItems,
+		CreatedAt:  &now,
+	}
+	err := h.Repo.Insert(r.Context(), order)
+	if err != nil {
+		fmt.Println("failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
 }
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("List all orders")
